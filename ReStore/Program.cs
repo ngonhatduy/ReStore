e795c2daf247ef07decefc871b8739a3ff44c31d
NgoNhatDuy <ngonhatduy240297@gmail.com>
2022-03-21 10:44:00 +0700

    Comment swagger setup

--- ReStore/Program.cs
+++ ReStore/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 
 //Add Connection
@@ -40,8 +40,8 @@ app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
 
